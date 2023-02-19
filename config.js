4f9cdd29790021bb9b571dfc7c90e0334ff27311
cloudlandsoul <125787968+cloudlandsoul@users.noreply.github.com>
2023-02-19 12:50:15 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  database: {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'mypassword',
+    database: 'mydatabase'
+  },
+  apiKeys: {
+    googleMaps: 'myapikey123',
+    stripe: 'anotherapikey456'
+  },
+  environment: 'development',
+  otherConfigValue: 12345
+}
